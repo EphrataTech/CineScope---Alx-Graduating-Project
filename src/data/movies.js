e9bd225ff7b52aae9222a0232a9ff32bf0a9056e
Ephrata <ephrata0327@gmail.com>
2025-12-20 17:58:55 +0300

    Add sample movie data for development
    - Create sample movies array with popular titles

---
+++ src/data/movies.js
@@ -0,0 +1,38 @@
+export const sampleMovies = [
+  {
+    id: 1,
+    title: "The Dark Knight",
+    year: "2008",
+    image: null
+  },
+  {
+    id: 2,
+    title: "Inception",
+    year: "2010",
+    image: null
+  },
+  {
+    id: 3,
+    title: "Interstellar",
+    year: "2014",
+    image: null
+  },
+  {
+    id: 4,
+    title: "The Matrix",
+    year: "1999",
+    image: null
+  },
+  {
+    id: 5,
+    title: "Pulp Fiction",
+    year: "1994",
+    image: null
+  },
+  {
+    id: 6,
+    title: "The Godfather",
+    year: "1972",
+    image: null
+  }
+]
