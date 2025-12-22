17a47575b5511e80a04c7734862275e7c899759d
Ephrata <ephrata0327@gmail.com>
2025-12-22 10:51:18 +0300

    Restructure App with routing system

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,16 @@
+import { Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
-import MovieCard from './components/MovieCard'
 import Footer from './components/Footer'
-import { sampleMovies } from './data/movies'
+import Home from './pages/Home'
 
 function App() {
   return (
     <div className="min-h-screen bg-gray-900 text-white">
       <Navbar />
-      <main className="container mx-auto p-4">
-        <h2 className="text-3xl font-bold mb-6">Popular Movies</h2>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-          {sampleMovies.map((movie) => (
-            <MovieCard 
-              key={movie.id} 
-              title={movie.title} 
-              year={movie.year} 
-              image={movie.image} 
-            />
-          ))}
-        </div>
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
       </main>
       <Footer />
     </div>
